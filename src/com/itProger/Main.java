06808dd993d69274044ec0607ec0cac47383b943
alshe43 <alyona_shelestova@bk.ru>
2022-09-03 20:59:55 +0700

    Sluchayno vidimo izmenila

--- src/com/itProger/Main.java
+++ src/com/itProger/Main.java
@@ -3,7 +3,7 @@ package com.itProger;
 public class Main {
 
     public static void Main(String[] args) {
-        System.out.println("Hello123");
+        System.out.println("Hello123git status");
 
     }
 }
